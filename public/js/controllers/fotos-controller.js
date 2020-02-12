3f9fef471a06c8b487789be492d526e6081dc0f8
Pedro Silva <pedro.silva@bomcontrole.com.br>
2020-02-12 12:05:34 -0300

    Iniciando binding com ng-repeat

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,20 @@
 angular.module('alurapic').controller("FotosController", function ($scope) {
 
-    $scope.foto = {
-        title: 'Teste',
-        url: 'https://lh3.googleusercontent.com/proxy/CDVnf8J2GJLtGY4UTc-LG1VTKGGJDv9QVAg9_7N3A-Jg7JjoTocYiaett6f8Sw9eIwcsnGIj5-3v4UBs8o98fG7ZFRom6Lht2YYVo7joltgL'
-    };
+    $scope.foto = {};
+
+    $scope.lstFotos = [{
+            title: 'Teste0',
+            url: 'https://lh3.googleusercontent.com/proxy/CDVnf8J2GJLtGY4UTc-LG1VTKGGJDv9QVAg9_7N3A-Jg7JjoTocYiaett6f8Sw9eIwcsnGIj5-3v4UBs8o98fG7ZFRom6Lht2YYVo7joltgL'
+        },
+
+        {
+            title: 'Teste1',
+            url: 'https://lh3.googleusercontent.com/proxy/CDVnf8J2GJLtGY4UTc-LG1VTKGGJDv9QVAg9_7N3A-Jg7JjoTocYiaett6f8Sw9eIwcsnGIj5-3v4UBs8o98fG7ZFRom6Lht2YYVo7joltgL'
+        },
+
+        {
+            title: 'Teste2',
+            url: 'https://lh3.googleusercontent.com/proxy/CDVnf8J2GJLtGY4UTc-LG1VTKGGJDv9QVAg9_7N3A-Jg7JjoTocYiaett6f8Sw9eIwcsnGIj5-3v4UBs8o98fG7ZFRom6Lht2YYVo7joltgL'
+        },
+    ]
 });
