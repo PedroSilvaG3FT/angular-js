1c9b62199eac69c165c14f21b1935cd2cc7e7fb8
Pedro Silva <pedro.silva@bomcontrole.com.br>
2020-02-12 16:06:06 -0300

    Validando se o formulario esta valido | Chamada post ajax

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,22 @@
 
-angular.module('alurapic').controller('FotoController', function ($scope) {
-        $scope.foto = {};
+angular.module('alurapic').controller('FotoController', function ($scope, $http) {
+    $scope.foto = {};
 
-        $scope.submeter = function () {
-            console.log("CADASTRO", $scope.foto);
-        };
+    $scope.submeter = function () {
+        console.log("CADASTRO", $scope.foto);
 
-    });
+        if ($scope.formulario.$valid) {
+
+            $http.post('v1/fotos', $scope.foto)
+                .success(function () {
+                    $scope.foto = {};
+                    console.log("FOTO CADASTRADA");
+                })
+                .error(function () {
+                    console.log("ERRO : ", error);
+                });
+        }
+
+    };
+
+});
