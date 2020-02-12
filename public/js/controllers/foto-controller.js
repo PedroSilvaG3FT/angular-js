765962413ffc05104aa8d006f642e36d6594689a
Pedro Silva <pedro.silva@bomcontrole.com.br>
2020-02-12 16:13:55 -0300

    Exibindo mensagens para usuario

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,6 +1,7 @@
 
 angular.module('alurapic').controller('FotoController', function ($scope, $http) {
     $scope.foto = {};
+    $scope.mensagem = "";
 
     $scope.submeter = function () {
         console.log("CADASTRO", $scope.foto);
@@ -10,9 +11,12 @@ angular.module('alurapic').controller('FotoController', function ($scope, $http)
             $http.post('v1/fotos', $scope.foto)
                 .success(function () {
                     $scope.foto = {};
+                    $scope.mensagem = "Foto incluida com sucesso";
                     console.log("FOTO CADASTRADA");
                 })
                 .error(function () {
+                    $scope.mensagem = "Não foi possível incluir a foto";
+
                     console.log("ERRO : ", error);
                 });
         }
