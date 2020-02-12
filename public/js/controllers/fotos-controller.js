2c96e9646a290cd8209be0598175ca379860649d
Pedro Silva <pedro.silva@bomcontrole.com.br>
2020-02-12 14:10:21 -0300

    Implementando filter de ng-repeat

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,11 +1,9 @@
 angular.module('alurapic').controller("FotosController", function ($scope, $http) {
 
-    $scope.foto = {};
-
     $scope.lstFotos = [];
+    $scope.filtro = "";
 
     var promise = $http.get('v1/fotos');
-
     promise
         .then(function (retorno) {
             $scope.lstFotos = retorno.data;
