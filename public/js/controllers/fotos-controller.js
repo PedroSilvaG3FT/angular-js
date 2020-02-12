25bad59c756f1b4f0768a1306355bb0af73f5df7
Pedro Silva <pedro.silva@bomcontrole.com.br>
2020-02-12 11:40:44 -0300

    Iniciando aplicaçao

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller("FotosController", function ($scope) {
+
+    $scope.foto = {
+        title: 'Teste',
+        url: 'https://lh3.googleusercontent.com/proxy/CDVnf8J2GJLtGY4UTc-LG1VTKGGJDv9QVAg9_7N3A-Jg7JjoTocYiaett6f8Sw9eIwcsnGIj5-3v4UBs8o98fG7ZFRom6Lht2YYVo7joltgL'
+    };
+});
