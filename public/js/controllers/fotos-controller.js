81d4ab644c4cd488ea0cbcd919a457c097d7cd0e
Pedro Silva <pedro.silva@bomcontrole.com.br>
2020-02-13 14:29:37 -0300

    Implementaçao de metodo de remover elemento

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,12 +3,25 @@ angular.module('alurapic').controller("FotosController", function ($scope, $http
     $scope.lstFotos = [];
     $scope.filtro = "";
 
-    var promise = $http.get('v1/fotos');
-    promise
+    $http.get('v1/fotos')
         .then(function (retorno) {
             $scope.lstFotos = retorno.data;
         })
         .catch(function (error) {
             console.log("ERRO : ", error);
         });
+
+    $scope.remover = function(foto) {
+        console.log("ID REMOVER : ", foto);
+
+        $http.delete('v1/fotos' + foto.id)
+            .then(function (retorno) {
+                var indiceFoto = $scope.lstFotos.indexOf(foto);
+                $scope.lstFotos.splice(indiceFoto, 1);
+            })
+            .catch(function (error) {
+                console.log("ERRO : ", error);
+            });
+    };
+
 });
